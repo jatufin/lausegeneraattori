bda8c641e074cf05cce1c80d2a64522c11e38885
Janne Tuukkanen <jatufin@gmail.com>
2021-11-13 23:56:02 +0200

    Add constructor to the main class

--- src/SentenceGenerator.py
+++ src/SentenceGenerator.py
@@ -2,17 +2,22 @@ class SentenceGenerator:
     """
     Generate random sentences based on given text using Markov chains
     """
+    def __init__(self, degree=5):
+        """ Argument: degree is the maximum Markov degree, which will be saved to the tree """
+        self.degree = degree
+
     def readText(self, filename):
+        """ Read and process the give text file """
         return filename
-    
+
     def save(self, filename):
+        """ Save the current data structure to binary file """
         return filename
-    
+
     def load(self, filename):
+        """ Load a previously saved binary file """
         return filename
-        
+
     def generate(self, degree, wordlist=[]):
         ws = "" if wordlist == [] else " " + " ".join(wordlist)
-        return degree + ws 
-    
-    
+        return degree + ws
--- src/tests/sentence_generator_test.py
+++ src/tests/sentence_generator_test.py
@@ -3,23 +3,21 @@ import unittest
 from SentenceGenerator import SentenceGenerator
 
 class TestSentenceGenerator(unittest.TestCase):
+    """ Test class for SentenceGenerator """
     def setUp(self):
         print("Setting up the test env")
 
     def test_foo(self):
         self.assertEqual("FOO", "FOO")
-        
+
     def test_methods(self):
         s = "TESTSTRING"
         sg = SentenceGenerator()
-        
+
         self.assertEqual(sg.readText(s), s)
         self.assertEqual(sg.save(s), s)
         self.assertEqual(sg.load(s), s)
 
         self.assertEqual(sg.generate(s), s)
         wordlist = ["aa", "bb"]
-        self.assertEqual(sg.generate(s, wordlist), s + " aa bb")
-        
-
-    
+        self.assertEqual(sg.generate(s, wordlist), s + " aa bb")  
