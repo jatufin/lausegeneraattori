61a18674a04207c6fb2445f9302c0864ff0749b1
Janne Tuukkanen <jatufin@gmail.com>
2021-11-13 23:05:01 +0200

    Create the project main class

---
+++ src/SentenceGenerator.py
@@ -0,0 +1,14 @@
+class SentenceGenerator:
+    """
+    Generate random sentences based on given text using Markov chains
+    """
+    def readText(self, filename):
+        pass
+    def save(self, filename):
+        pass
+    def load(self, filename):
+        pass
+    def generate(self, degree, wordlist=[]):
+        pass
+    
+    
