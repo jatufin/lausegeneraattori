e94b249335b2e8048a460bb99fdfb0643de710fc
Janne Tuukkanen <jatufin@gmail.com>
2021-11-27 14:43:42 +0200

    Alustettiin testikattavuuden raportointi

--- tasks.py
+++ tasks.py
@@ -3,3 +3,15 @@ from invoke import task
 @task
 def start(ctx):
     ctx.run("python3 src/sentence_generator.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
